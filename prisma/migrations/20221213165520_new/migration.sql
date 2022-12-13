bac7c50c96ca29ab34503789a5bcfd8e95eda59f
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-13 17:56:26 +0100

    Add user to db

---
+++ prisma/migrations/20221213165520_new/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `User` DROP PRIMARY KEY,
+    ADD COLUMN `email` VARCHAR(191) NOT NULL,
+    ADD COLUMN `role` ENUM('BASIC', 'ADMIN') NOT NULL DEFAULT 'BASIC',
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
+
+-- CreateIndex
+CREATE INDEX `User_email_idx` ON `User`(`email`);
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -2,8 +2,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.findMany()
+  await prisma.user.deleteMany()
+  const user = await prisma.user.create({
+    data: {
+      name: "Johanna",
+      email: "johanna@test.com"
+    }
+  })
   console.log(user)
+  // const user = await prisma.user.findMany()
 
   // await prisma.user.update({ 
   //   where: { id: 2 },
