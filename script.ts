50da0f865f224999304f0ec6f610bedc945c1551
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-13 18:03:09 +0100

    Find unique

--- script.ts
+++ script.ts
@@ -2,15 +2,21 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.user.deleteMany()
-  const user = await prisma.user.create({
-    data: {
-      name: "Johanna",
+  // await prisma.user.deleteMany()
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Johanna",
+  //     email: "johanna@test.com"
+  //   }
+  // })
+
+  const user = await prisma.user.findUnique({
+    where: {
       email: "johanna@test.com"
     }
   })
+
   console.log(user)
-  // const user = await prisma.user.findMany()
 
   // await prisma.user.update({ 
   //   where: { id: 2 },
