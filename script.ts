fcd1fd098ec1f6fb83709b21b7a685fa4d4ed2c7
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-13 17:10:18 +0100

    Create prisma

---
+++ script.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.findMany()
+  console.log(user)
+
+  // await prisma.user.update({ 
+  //   where: { id: 2 },
+  //   data: {
+  //     name: "John"
+  //   }
+  // })
+}
+
+main()
+  .catch(e => {
+    console.error(e.message)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
